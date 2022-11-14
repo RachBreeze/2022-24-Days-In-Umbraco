73a6c796bc394b15c1b578ffb774594829232858
Rachel Breeze <rachel.breeze@sigma.se>
2022-11-14 21:00:50 +0000

    Added licence information

--- PetAPI/Program.cs
+++ PetAPI/Program.cs
@@ -13,11 +13,25 @@ namespace PetAPI
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
             var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Pet Store API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Pet Store API",
+                    Version = "v1",
+                    Contact = new OpenApiContact
+                    {
+                        Email = "testemail@email.me",
+                        Name = "Rachel Breeze"
+                    },
+                    Description = "This is a sample Pet Store Server based on the OpenAPI 3.0 specification.",
+                    License = new OpenApiLicense
+                    {
+                        Name = "Apache 2.0",
+                        Url =new Uri("http://www.apache.org/licenses/LICENSE-2.0.html")
+                    }
+                });
                 c.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
             });
 
