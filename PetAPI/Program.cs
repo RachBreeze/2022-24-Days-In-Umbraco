ed2cc973cb3fe6699e247d01fb7da6d5bbfbdf73
Rachel Breeze <rachel.breeze@sigma.se>
2022-11-14 20:21:41 +0000

    Added Swagger middleware and documentation settings.

--- PetAPI/Program.cs
+++ PetAPI/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 namespace PetAPI
 {
     public class Program
@@ -5,10 +8,32 @@ namespace PetAPI
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Pet Store API", Version = "v1" });
+                c.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
+            });
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseHttpsRedirection();
 
+            app.UseAuthorization();
+
+
+            app.MapControllers();
             app.Run();
         }
     }
