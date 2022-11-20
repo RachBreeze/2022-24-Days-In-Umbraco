91c28c80c784e3f9d39c4dd0491c90d2858b9c14
Rachel Breeze <rachel.breeze@sigma.se>
2022-11-20 11:12:18 +0000

    Added support for petcontroller

---
+++ PetAPI/Controllers/PetController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using PetApi.Model;
+using System.ComponentModel.DataAnnotations;
+
+namespace PetAPI.Controllers
+{
+    /// <summary>
+    /// Everything about your Pets
+    /// </summary>
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class PetController : ControllerBase
+    {
+        /// <summary>
+        /// Update an existing  pet by id
+        /// </summary>
+        [HttpPut]
+        public IActionResult Update([FromBody]Pet request)
+        {
+            return null;
+        }
+
+    }
+}
---
+++ PetAPI/Model/Pet.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetApi.Model
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public Category Category { get; set; }
+
+        public string[] PhotoUrls { get; set; }
+
+        public ICollection<Tag> Tags { get; set; }
+        public string Status { get; set; }
+    }
+}
